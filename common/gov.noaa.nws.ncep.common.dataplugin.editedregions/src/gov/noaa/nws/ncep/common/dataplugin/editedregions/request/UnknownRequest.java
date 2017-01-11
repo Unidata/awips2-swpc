995fb61cf3e7ba27977fc8cbe2c50fc49e08e886
Ada Lockleigh <Ada.Lockleigh@noaa.gov>
2017-01-11 18:27:57 +0000

    Corrected compilation problems and removed extraneous class files.
    Change-Id: I50524dd7379f83cc2ec24488470446d58e3e0ab8

--- common/gov.noaa.nws.ncep.common.dataplugin.editedregions/src/gov/noaa/nws/ncep/common/dataplugin/editedregions/request/UnknownRequest.java
+++ common/gov.noaa.nws.ncep.common.dataplugin.editedregions/src/gov/noaa/nws/ncep/common/dataplugin/editedregions/request/UnknownRequest.java
@@ -3,9 +3,8 @@ package gov.noaa.nws.ncep.common.dataplugin.editedregions.request;
 import gov.noaa.nws.ncep.common.dataplugin.editedregions.request.intf.IRequest;
 
 /**
- * Request class used if the request supplied to the gateway
- * is null or the request is of a type that the RequestHandler
- * does not recognize.
+ * Request class used if the request supplied to the gateway is null or the
+ * request is of a type that the RequestHandler does not recognize.
  * 
  * 
  * <pre>
@@ -21,24 +20,25 @@ import gov.noaa.nws.ncep.common.dataplugin.editedregions.request.intf.IRequest;
  * @author jtravis
  * @version 1.0
  */
-public class UnknownRequest extends Request<ProcessEventsRequest> implements IRequest {
+public class UnknownRequest extends Request<UnknownRequest>
+        implements IRequest {
 
-	private long id = 0L;
-	
-	/**
-	 * 
-	 */
-	public UnknownRequest() {
-		this.id = System.currentTimeMillis();
-	}
+    private long id = 0L;
 
-	@Override
-	public long getId() {
-		return this.id;
-	}
+    /**
+     * 
+     */
+    public UnknownRequest() {
+        this.id = System.currentTimeMillis();
+    }
 
-	@Override
-	public void setId(long Id) {
+    @Override
+    public long getId() {
+        return this.id;
+    }
+
+    @Override
+    public void setId(long Id) {
         // The id is set when the request is initialized
         // but the system still requires the method for
         // serialization / deserialization through the gateway.
@@ -48,10 +48,10 @@ public class UnknownRequest extends Request<ProcessEventsRequest> implements IRe
         // TODO - verify this
         ;
         ;
-	}
+    }
 
-	@Override
-	public boolean isValid() {
-		return true;
-	}
+    @Override
+    public boolean isValid() {
+        return true;
+    }
 }
--- common/gov.noaa.nws.ncep.common.dataplugin.editedregions/src/gov/noaa/nws/ncep/common/dataplugin/editedregions/response/SaveResponse.java
+++ common/gov.noaa.nws.ncep.common.dataplugin.editedregions/src/gov/noaa/nws/ncep/common/dataplugin/editedregions/response/SaveResponse.java
@@ -10,10 +10,10 @@ import gov.noaa.nws.ncep.common.dataplugin.editedregions.response.intf.IResponse
 import gov.noaa.nws.ncep.common.dataplugin.editedregions.results.intf.IResults;
 
 /**
- * Class representing the response returned when an
- * instance of a SaveCommand is executed.
+ * Class representing the response returned when an instance of a SaveCommand is
+ * executed.
  * 
- *  <pre>
+ * <pre>
  * 
  * SOFTWARE HISTORY
  * 
@@ -27,12 +27,13 @@ import gov.noaa.nws.ncep.common.dataplugin.editedregions.results.intf.IResults;
  * @version 1.0
  */
 @DynamicSerialize
-public class SaveResponse extends BaseResponse<ProcessEventsResponse> implements IResponse {
+public class SaveResponse extends BaseResponse<SaveResponse>
+        implements IResponse {
 
-	/**
-	 * The error
-	 */
-	@DynamicSerializeElement
+    /**
+     * The error
+     */
+    @DynamicSerializeElement
     private EditedRegionsException error = null;
 
     /**
@@ -40,7 +41,7 @@ public class SaveResponse extends BaseResponse<ProcessEventsResponse> implements
      */
     @DynamicSerializeElement
     private IResults results = null; // TODO find out what the vector
-                                           // should hold
+                                     // should hold
 
     /**
      * Time in milliseconds to complete round trip of request / response
@@ -59,124 +60,178 @@ public class SaveResponse extends BaseResponse<ProcessEventsResponse> implements
      */
     @DynamicSerializeElement
     private SaveRequest request;
-	
-	/**
-	 * 
-	 */
-	public SaveResponse() {
-		// TODO Auto-generated constructor stub
-	}
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#getError()
-	 */
-	@Override
-	public EditedRegionsException getError() {
-		return this.error;
-	}
+    /**
+     * 
+     */
+    public SaveResponse() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#setError(gov.noaa.nws.ncep.common.dataplugin.editedevents.exception.EditedEventsException)
-	 */
-	@Override
-	public void setError(EditedRegionsException exception) {
-		this.error = exception;
-	}
-
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#getResults()
-	 */
-	@Override
-	public IResults getResults() {
-		return this.results;
-	}
-
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#setResults(java.util.Vector)
-	 */
-	@Override
-	public void setResults(IResults results) {
-		this.results = results;
-	}
-
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#getResponseTime()
-	 */
-	@Override
-	public long getResponseTime() {
-		return this.responseTime;
-	}
-
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#setResponseTime(long)
-	 */
-	@Override
-	public void setResponseTime(long timeInMillis) {
-		this.responseTime = timeInMillis;
-	}
-
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#getProcessingTime()
-	 */
-	@Override
-	public long getProcessingTime() {
-		return this.processingTime;
-	}
-
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#setProcessingTime(long)
-	 */
-	@Override
-	public void setProcessingTime(long timeInMillis) {
-		this.processingTime = timeInMillis;
-	}
-
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#getRequest()
-	 */
-	@Override
-	public SaveRequest getRequest() {
-		return this.request;
-	}
-
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#setRequest(gov.noaa.nws.ncep.common.dataplugin.editedevents.request.intf.IRequest)
-	 */
-	@Override
-	public void setRequest(IRequest request) {
-		this.request = (SaveRequest) request;
-	}
-	
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#hasErrors()
-	 */
-	@Override
-	public boolean hasErrors() {
-		if (this.error == null) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	/* (non-Javadoc)
-     * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#hasResults()
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * getError()
      */
     @Override
-	public boolean hasResults() {
+    public EditedRegionsException getError() {
+        return this.error;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * setError(gov.noaa.nws.ncep.common.dataplugin.editedevents.exception.
+     * EditedEventsException)
+     */
+    @Override
+    public void setError(EditedRegionsException exception) {
+        this.error = exception;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * getResults()
+     */
+    @Override
+    public IResults getResults() {
+        return this.results;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * setResults(java.util.Vector)
+     */
+    @Override
+    public void setResults(IResults results) {
+        this.results = results;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * getResponseTime()
+     */
+    @Override
+    public long getResponseTime() {
+        return this.responseTime;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * setResponseTime(long)
+     */
+    @Override
+    public void setResponseTime(long timeInMillis) {
+        this.responseTime = timeInMillis;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * getProcessingTime()
+     */
+    @Override
+    public long getProcessingTime() {
+        return this.processingTime;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * setProcessingTime(long)
+     */
+    @Override
+    public void setProcessingTime(long timeInMillis) {
+        this.processingTime = timeInMillis;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * getRequest()
+     */
+    @Override
+    public SaveRequest getRequest() {
+        return this.request;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * setRequest(gov.noaa.nws.ncep.common.dataplugin.editedevents.request.intf.
+     * IRequest)
+     */
+    @Override
+    public void setRequest(IRequest request) {
+        this.request = (SaveRequest) request;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * hasErrors()
+     */
+    @Override
+    public boolean hasErrors() {
+        if (this.error == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * hasResults()
+     */
+    @Override
+    public boolean hasResults() {
         if (this.results == null) {
             return false;
         } else {
             return true;
         }
-	}
-    
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#setMessage(java.lang.String)
-	 */
-	@Override
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.common.dataplugin.editedevents.response.intf.IResponse#
+     * setMessage(java.lang.String)
+     */
+    @Override
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
 }
