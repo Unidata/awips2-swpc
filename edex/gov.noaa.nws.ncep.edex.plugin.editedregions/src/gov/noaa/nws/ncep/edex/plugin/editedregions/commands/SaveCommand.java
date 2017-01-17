ec4812543271995b0f600d8cf26232694a4743b8
Ada Lockleigh <Ada.Lockleigh@noaa.gov>
2017-01-17 21:23:11 +0000

    Fixed compile errors from previous commits.
    Change-Id: I388f19f9566d99693bc5a02e10169e2cf79c086d

--- edex/gov.noaa.nws.ncep.edex.plugin.editedregions/src/gov/noaa/nws/ncep/edex/plugin/editedregions/commands/SaveCommand.java
+++ edex/gov.noaa.nws.ncep.edex.plugin.editedregions/src/gov/noaa/nws/ncep/edex/plugin/editedregions/commands/SaveCommand.java
@@ -3,12 +3,10 @@ package gov.noaa.nws.ncep.edex.plugin.editedregions.commands;
 import gov.noaa.nws.ncep.common.dataplugin.editedregions.exception.EditedRegionsException;
 import gov.noaa.nws.ncep.common.dataplugin.editedregions.request.SaveRequest;
 import gov.noaa.nws.ncep.common.dataplugin.editedregions.request.intf.IRequest;
-import gov.noaa.nws.ncep.common.dataplugin.editedregions.response.AddEventResponse;
 import gov.noaa.nws.ncep.common.dataplugin.editedregions.response.intf.IResponse;
 
 /**
- * The command class that is executed to save any
- * changes
+ * The command class that is executed to save any changes
  * 
  * 
  * <pre>
@@ -22,7 +20,7 @@ import gov.noaa.nws.ncep.common.dataplugin.editedregions.response.intf.IResponse
  * </pre>
  *
  * @author jtravis
- * @version 1.0	
+ * @version 1.0
  */
 public class SaveCommand extends BaseCommand {
 
@@ -31,120 +29,150 @@ public class SaveCommand extends BaseCommand {
      * command
      */
     private SaveRequest request = null;
-	/**
-	 * 
-	 */
-	public SaveCommand() {
-	}
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#getError()
-	 */
-	@Override
-	public EditedEventsException getError() {
-		return this.error;
-	}
+    /**
+     * 
+     */
+    public SaveCommand() {
+    }
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#hasError()
-	 */
-	@Override
-	public boolean hasError() {
-		if (this.error == null) {
-			return false;
-		} else {
-			return true;
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#
+     * getError()
+     */
+    @Override
+    public EditedRegionsException getError() {
+        return this.error;
+    }
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#getStartTime()
-	 */
-	@Override
-	public long getStartTime() {
-		return this.startTime;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#
+     * hasError()
+     */
+    @Override
+    public boolean hasError() {
+        if (this.error == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#getEndTime()
-	 */
-	@Override
-	public long getEndTime() {
-		return this.endTime;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#
+     * getStartTime()
+     */
+    @Override
+    public long getStartTime() {
+        return this.startTime;
+    }
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#getProcessingTime()
-	 */
-	@Override
-	public long getProcessingTime() {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#
+     * getEndTime()
+     */
+    @Override
+    public long getEndTime() {
+        return this.endTime;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#
+     * getProcessingTime()
+     */
+    @Override
+    public long getProcessingTime() {
         long time = 0L;
 
         time = this.getEndTime() - this.getStartTime();
 
         return time;
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#isValid()
-	 */
-	@Override
-	public boolean isValid() {
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#isValid
+     * ()
+     */
+    @Override
+    public boolean isValid() {
+        return false;
+    }
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#setRequest(gov.noaa.nws.ncep.common.dataplugin.editedevents.request.intf.IRequest)
-	 */
-	@Override
-	public void setRequest(IRequest request) {
-		this.request = (SaveRequest) request;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#
+     * setRequest(gov.noaa.nws.ncep.common.dataplugin.editedevents.request.intf.
+     * IRequest)
+     */
+    @Override
+    public void setRequest(IRequest request) {
+        this.request = (SaveRequest) request;
+    }
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#getRequest()
-	 */
-	@Override
-	public SaveRequest getRequest() {
-		return this.request;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#
+     * getRequest()
+     */
+    @Override
+    public SaveRequest getRequest() {
+        return this.request;
+    }
 
-	/* (non-Javadoc)
-	 * @see gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#execute()
-	 */
-	@Override
-	public IResponse execute() {
-		
-		this.setStartTime();
-		
-		// TODO do something here
-		
-		this.setEndTime();
-		
-		return this.createResponse();
-		
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gov.noaa.nws.ncep.edex.plugin.editedevents.commands.intf.ICommand#execute
+     * ()
+     */
+    @Override
+    public IResponse execute() {
 
-	/**
-	 * Create the response
-	 * 
-	 * @return IResponse
-	 */
-	private IResponse createResponse() { 
-		
-		AddEventResponse response = new AddEventResponse();
-		
-		// populate the response and the results
-        if (this.hasError()) {
-        	response.setError(this.getError());
-        }        
-        
-        response.setRequest(this.getRequest()); 
-        response.setProcessingTime(this.getProcessingTime());
-        
-        return response;
-		
-	}
+        this.setStartTime();
 
+        // TODO do something here
+
+        this.setEndTime();
+
+        return this.createResponse();
+
+    }
+
+    /**
+     * Create the response
+     * 
+     * @return IResponse
+     */
+    private IResponse createResponse() {
+
+        // AddEventResponse response = new AddEventResponse();
+        //
+        // // populate the response and the results
+        // if (this.hasError()) {
+        // response.setError(this.getError());
+        // }
+        //
+        // response.setRequest(this.getRequest());
+        // response.setProcessingTime(this.getProcessingTime());
+        //
+        // return response;
+        return null;
+    }
 
 }
