08a0d483ed89b10957408daea1aa46fb85e537d0
Ada Lockleigh <Ada.Lockleigh@noaa.gov>
2017-05-12 19:40:39 +0000

    Added observatory and type field to the EnterRegionReportDialog

--- viz/gov.noaa.nws.ncep.viz.ui.editedregions/src/gov/noaa/nws/ncep/viz/ui/editedregions/dialog/EnterRegionReportDialog.java
+++ viz/gov.noaa.nws.ncep.viz.ui.editedregions/src/gov/noaa/nws/ncep/viz/ui/editedregions/dialog/EnterRegionReportDialog.java
@@ -4,15 +4,63 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
 
 public class EnterRegionReportDialog extends Dialog {
+
+    // Input fields
+    private Combo cmbObservatory;
+
+    private Text txtType;
+
+    private Combo cmbQuality;
+
+    private Text txtRegion;
+
+    private Text txtLatitude;
+
+    private Text txtReportLongitude;
+
+    private Text txtLongitude;
+
+    private Text txtReportLocation;
+
+    private Text txtLocation;
+
+    private Text txtCarlon;
+
+    private Text txtExtent;
+
+    private Text txtArea;
+
+    private Text txtNumspots;
+
+    private Combo cmbZurich;
+
+    private Combo cmbPenumbra;
+
+    private Combo cmbCompact;
+
+    private Text txtSpotclass;
+
+    private Combo cmbMagcode;
+
+    private Combo cmbMagclass;
+
+    private Combo cmbObsid;
+
+    private Combo cmbReportStatus;
+
+    private Combo cmbValidSpotClass;
 
     @SuppressWarnings("unused")
     private final IUFStatusHandler statusHandler = UFStatus
@@ -55,8 +103,24 @@ public class EnterRegionReportDialog extends Dialog {
 
     }
 
-    private void createFieldControls(Group bottomGroup) {
-        // TODO: add implementation
+    private void createFieldControls(Group group) {
+
+        Composite composite = new Composite(group, SWT.None);
+        composite.setLayout(new GridLayout(3, false));
+        composite.setLayoutData(
+                new GridData(GridData.FILL, GridData.FILL, true, true));
+
+        // Observatory field
+        new Label(composite, SWT.LEFT).setText("Observatory: ");
+        cmbObservatory = new Combo(composite, SWT.DROP_DOWN ^ SWT.READ_ONLY);
+
+        new Label(composite, SWT.LEFT).setText(" ");
+
+        // Type field
+        new Label(composite, SWT.LEFT).setText("Type: ");
+        txtType = new Text(composite, SWT.BORDER);
+
+        new Label(composite, SWT.LEFT).setText(" ");
     }
 
     private void initializeControlsWithDefaultValues() {
