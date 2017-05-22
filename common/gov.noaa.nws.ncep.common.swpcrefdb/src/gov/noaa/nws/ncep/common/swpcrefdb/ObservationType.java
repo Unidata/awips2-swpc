d80c38c5c2260c588142a43f4cc32bbf5dd54a67
Jason Travis <Jason.Travis@noaa.gov>
2017-05-22 13:36:15 -0400

    creating ref tables

---
+++ common/gov.noaa.nws.ncep.common.swpcrefdb/src/gov/noaa/nws/ncep/common/swpcrefdb/ObservationType.java
@@ -0,0 +1,113 @@
+package gov.noaa.nws.ncep.common.swpcrefdb;
+
+import gov.noaa.nws.ncep.common.swpcrefdb.intf.ISWPCBaseTable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import com.raytheon.uf.common.dataplugin.persist.PersistableDataObject;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
+
+/**
+ * Class representing the SWPC_STATION table.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jan 22, 2016 R9583      jtravis     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author jtravis
+ * @version 1.0
+ */
+@SuppressWarnings("rawtypes")
+@DynamicSerialize
+@Entity
+@Table(name = "SWPC_OBSERVATION_TYPE")
+public class ObservationType extends PersistableDataObject implements ISWPCBaseTable {
+
+    private static final long serialVersionUID = -2052838874371396178L;   
+
+    public static final String ID = "id"; 
+
+    public static final String DESCRIPTION = "description"; 
+
+    public static final String TYPE = "type";
+    
+    @Id
+    @DynamicSerializeElement
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "swpc_observation_type_seq_gen")
+    @SequenceGenerator(name = "swpc_observation_type_seq_gen", sequenceName = "SWPC_OBSERVATION_TYPE_SEQ")
+    private long id = 0;
+
+
+    @Column(name = "TYPE", unique = true, nullable = false)
+    @DynamicSerializeElement
+    private Integer type = null;
+
+    @Embedded
+    @ManyToOne(cascade = { CascadeType.REFRESH })
+    @PrimaryKeyJoinColumn
+    @DynamicSerializeElement
+    private StationType description;
+
+    /**
+	 * 
+	 */
+    public ObservationType() {
+    }
+
+	@Override
+	public long getId() {
+		return this.id;
+	}
+
+	@Override
+	public void setId(long id) {
+		this.id = id;
+		
+	}
+
+	/**
+	 * @return the type
+	 */
+	public Integer getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public StationType getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(StationType description) {
+		this.description = description;
+	}
+
+}
