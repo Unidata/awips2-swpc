75323f9a22197a2613074b99a948a993bfb4f82c
Ada Lockleigh <Ada.Lockleigh@noaa.gov>
2017-06-05 14:53:22 +0000

    Updated code so that report ids are transmitted back to the client.

--- edex/gov.noaa.nws.ncep.edex.plugin.editedregions/src/gov/noaa/nws/ncep/edex/plugin/editedregions/commands/GetRegionReportsCommand.java
+++ edex/gov.noaa.nws.ncep.edex.plugin.editedregions/src/gov/noaa/nws/ncep/edex/plugin/editedregions/commands/GetRegionReportsCommand.java
@@ -1,7 +1,9 @@
 package gov.noaa.nws.ncep.edex.plugin.editedregions.commands;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.raytheon.uf.common.dataplugin.PluginException;
 import com.raytheon.uf.common.status.IUFStatusHandler;
@@ -17,9 +19,8 @@ import gov.noaa.nws.ncep.common.dataplugin.editedregions.results.GetRegionReport
 import gov.noaa.nws.ncep.edex.plugin.editedregions.dao.RegionReportsDao;
 
 /**
- * The command class that is executed to obtain all
- * region reports regardless of if they have been assigned
- * or unassigned to a specific region
+ * The command class that is executed to obtain all region reports regardless of
+ * if they have been assigned or unassigned to a specific region
  * 
  * 
  * @author jtravis
@@ -32,12 +33,12 @@ public class GetRegionReportsCommand extends BaseCommand {
      * command
      */
     private GetRegionReportsRequest request = null;
-    
+
     /**
      * Dao for Region Report records
      */
     private RegionReportsDao regionReportsDao = null;
-    
+
     /**
      * Logger
      */
@@ -167,42 +168,52 @@ public class GetRegionReportsCommand extends BaseCommand {
      */
     @Override
     public IResponse execute() {
-    	
-    	this.setStartTime();
-    	
+
+        this.setStartTime();
+
         List<RegionReport> unAssignedRegionReports = new ArrayList<>();
         List<RegionReport> assignedRegionReports = new ArrayList<>();
         GetRegionReportsResponse response = new GetRegionReportsResponse();
         GetRegionReportsResults results = new GetRegionReportsResults();
-        
+
         try {
-        
-        	regionReportsDao = new RegionReportsDao();
 
-        	if (this.request.isObtainAssignedReports()) {
-     
-        		assignedRegionReports = regionReportsDao.getAssignedRegionReports(true);
-        		
-        		results.setAssignedRegionReports(assignedRegionReports);
-        	}
-        
-        	if (this.request.isObtainUnassignedReports()) {
-        	
-        		unAssignedRegionReports = regionReportsDao.getUnAssignedRegionReports(true);
-        		
-        		results.setUnAssignedReports(unAssignedRegionReports);
-        	}
+            regionReportsDao = new RegionReportsDao();
 
-        	response.setResults(results);
-        	
+            if (this.request.isObtainAssignedReports()) {
+
+                assignedRegionReports = regionReportsDao
+                        .getAssignedRegionReports(true);
+
+                Map<Integer, RegionReport> assignedReportMap = new LinkedHashMap<>();
+                for (RegionReport report : assignedRegionReports) {
+                    assignedReportMap.put(report.getId(), report);
+                }
+
+                results.setAssignedRegionReports(assignedReportMap);
+            }
+            if (this.request.isObtainUnassignedReports()) {
+
+                unAssignedRegionReports = regionReportsDao
+                        .getUnAssignedRegionReports(true);
+
+                Map<Integer, RegionReport> unassignedReportMap = new LinkedHashMap<>();
+                for (RegionReport report : unAssignedRegionReports) {
+                    unassignedReportMap.put(report.getId(), report);
+                }
+
+                results.setUnAssignedReports(unassignedReportMap);
+            }
+
+            response.setResults(results);
 
         } catch (PluginException e) {
-        	EditedRegionsException erx = new EditedRegionsException(e);
-        	response.setError(erx);
+            EditedRegionsException erx = new EditedRegionsException(e);
+            response.setError(erx);
         }
-        
+
         this.setEndTime();
-        
+
         return response;
     }
 
