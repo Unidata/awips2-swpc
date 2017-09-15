c3ace1dc34b8a61a7b99c9d0a5c74037dc144d84
Ada Lockleigh <Ada.Lockleigh@noaa.gov>
2017-09-15 14:55:00 +0000

    Added getters and setters to GetConsensusTodaysResults.
    Change-Id: If4127bcefe0151f386ab41eb794bb51711827b18

--- common/gov.noaa.nws.ncep.common.dataplugin.editedregions/src/gov/noaa/nws/ncep/common/dataplugin/editedregions/results/GetConsensusTodaysResults.java
+++ common/gov.noaa.nws.ncep.common.dataplugin.editedregions/src/gov/noaa/nws/ncep/common/dataplugin/editedregions/results/GetConsensusTodaysResults.java
@@ -1,7 +1,8 @@
 package gov.noaa.nws.ncep.common.dataplugin.editedregions.results;
 
+import java.util.Date;
+
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
-import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 
 import gov.noaa.nws.ncep.common.dataplugin.editedregions.results.intf.IResults;
 
@@ -23,8 +24,29 @@ import gov.noaa.nws.ncep.common.dataplugin.editedregions.results.intf.IResults;
 @DynamicSerialize
 public class GetConsensusTodaysResults implements IResults {
 
-    @DynamicSerializeElement
-    private Long createdRegion = null;
+    private Date observationTime;
+
+    private String observatory;
+
+    private Integer quality;
+
+    private Integer region;
+
+    private Integer latitude;
+
+    private Integer longitude;
+
+    private Integer carlon;
+
+    private Integer extent;
+
+    private Integer area;
+
+    private Integer numspots;
+
+    private String spotclass;
+
+    private String magclass;
 
     /**
      * Constructor
@@ -42,26 +64,187 @@ public class GetConsensusTodaysResults implements IResults {
      */
     @Override
     public int numResults() {
-        if (this.createdRegion == null) {
-            return 0;
-        } else {
-            return 1;
-        }
+        return 0;
     }
 
     /**
-     * @return the createdRegion
+     * @return the observationTime
      */
-    public Long getCreatedRegion() {
-        return this.createdRegion;
+    public Date getObservationTime() {
+        return observationTime;
     }
 
     /**
-     * @param createdRegion
-     *            the createdRegion to set
+     * @param observationTime
+     *            the observationTime to set
      */
-    public void setCreatedRegion(Long createdRegion) {
-        this.createdRegion = createdRegion;
+    public void setObservationTime(Date observationTime) {
+        this.observationTime = observationTime;
+    }
+
+    /**
+     * @return the observatory
+     */
+    public String getObservatory() {
+        return observatory;
+    }
+
+    /**
+     * @param observatory
+     *            the observatory to set
+     */
+    public void setObservatory(String observatory) {
+        this.observatory = observatory;
+    }
+
+    /**
+     * @return the quality
+     */
+    public Integer getQuality() {
+        return quality;
+    }
+
+    /**
+     * @param quality
+     *            the quality to set
+     */
+    public void setQuality(Integer quality) {
+        this.quality = quality;
+    }
+
+    /**
+     * @return the region
+     */
+    public Integer getRegion() {
+        return region;
+    }
+
+    /**
+     * @param region
+     *            the region to set
+     */
+    public void setRegion(Integer region) {
+        this.region = region;
+    }
+
+    /**
+     * @return the latitude
+     */
+    public Integer getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * @param latitude
+     *            the latitude to set
+     */
+    public void setLatitude(Integer latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * @return the longitude
+     */
+    public Integer getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * @param longitude
+     *            the longitude to set
+     */
+    public void setLongitude(Integer longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     * @return the carlon
+     */
+    public Integer getCarlon() {
+        return carlon;
+    }
+
+    /**
+     * @param carlon
+     *            the carlon to set
+     */
+    public void setCarlon(Integer carlon) {
+        this.carlon = carlon;
+    }
+
+    /**
+     * @return the extent
+     */
+    public Integer getExtent() {
+        return extent;
+    }
+
+    /**
+     * @param extent
+     *            the extent to set
+     */
+    public void setExtent(Integer extent) {
+        this.extent = extent;
+    }
+
+    /**
+     * @return the area
+     */
+    public Integer getArea() {
+        return area;
+    }
+
+    /**
+     * @param area
+     *            the area to set
+     */
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    /**
+     * @return the numspots
+     */
+    public Integer getNumspots() {
+        return numspots;
+    }
+
+    /**
+     * @param numspots
+     *            the numspots to set
+     */
+    public void setNumspots(Integer numspots) {
+        this.numspots = numspots;
+    }
+
+    /**
+     * @return the spotclass
+     */
+    public String getSpotclass() {
+        return spotclass;
+    }
+
+    /**
+     * @param spotclass
+     *            the spotclass to set
+     */
+    public void setSpotclass(String spotclass) {
+        this.spotclass = spotclass;
+    }
+
+    /**
+     * @return the magclass
+     */
+    public String getMagclass() {
+        return magclass;
+    }
+
+    /**
+     * @param magclass
+     *            the magclass to set
+     */
+    public void setMagclass(String magclass) {
+        this.magclass = magclass;
     }
 
 }
